0652275bd707636ca45f4f5cc2231baefb162c0b
Joachim Eide <JoachimEide@Joachims-MacBook-Pro-2.local>
2019-04-07 22:55:43 +0200

    Animasjoner som blir trigget av scroll posisjoner.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-document.addEventListener('scroll', function(e){
-  const target = document.querySelectorAll('.scroll');
 
-  for (let index = 0; index < target.length; index++){
-    let pos = window.pageYOffsett * target[index].dataset.rate;
-    target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
-
-    console.log(target[index].style.transform);
+window.addEventListener("scroll", function(){
+  let offset = window.pageYOffset;
+  //console.log('Offset:' + offset);
+  if (offset > 375){
+    var morphing = anime({
+      targets: '#morphing .polymorph',
+      points: [
+        { value: '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369' },
+        { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+        { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+        { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+      ],
+      easing: 'easeOutQuad',
+      duration: 2000,
+      loop: false,
+    });
+  }
+  if (offset > 539){
+    var morphing = anime({
+      targets: '.st0',
+      y: '198',
+      height: '410',
+      easing: 'easeOutQuad',
+      duration: 2000,
+      loop: false,
+    })
   }
 })
